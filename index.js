ab93e085c1bb9fc9066c669fac5dd467987650d8
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-29 19:54:10 -0600

    We create the Routing and Controllers for authentication

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use( express.json() );
 
 // *Rutas de la APP
 app.use( '/api/usuarios', require( './routes/usuarios' ) );
+app.use( '/api/auth', require( './routes/auth' ) );
 
 // Todo: Arranca la app
 app.listen( port, () => {
