f7909f2ddbd87494dcce055641894775eb1dd995
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-30 15:09:32 -0600

    we add validation to our link route

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,9 +1,14 @@
 const Enlaces = require( '../models/Enlace' );
 const shortid = require( 'shortid' );
 const bcrypt  = require( 'bcrypt' );
+const { validationResult } = require( 'express-validator' );
 
 exports.nuevoEnlace = async ( req, res, next ) => {
     // Todo revisar si hay errores
+    const errores = validationResult( req );
+    if( !errores.isEmpty() ) {
+        return res.status( 400 ).json({ errores: errores.array() });
+    }
 
     //Todo: Crea un nuevo Objeto de Enlace
     const { nombre_original } = req.body;
