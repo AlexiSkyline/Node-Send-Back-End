7fbece49a9268eadace8ee4609d4bfb49880b66d
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-30 15:03:01 -0600

    add the function to define a password and the number of downloads of the links

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,22 +1,32 @@
 const Enlaces = require( '../models/Enlace' );
 const shortid = require( 'shortid' );
+const bcrypt  = require( 'bcrypt' );
 
 exports.nuevoEnlace = async ( req, res, next ) => {
     // Todo revisar si hay errores
 
     //Todo: Crea un nuevo Objeto de Enlace
-    const { nombre_original, password } = req.body;
+    const { nombre_original } = req.body;
     const enlace = new Enlaces();
     enlace.url = shortid.generate();
     enlace.nombre = shortid.generate();
     enlace.nombre_original = nombre_original;
-    enlace.password = password;
+
+    if( req.usuario ) {
+        const { password, descargas } = req.body;
+
+        enlace.descargas = ( descargas ) ? descargas: 1;
+
+        const salt = await bcrypt.genSalt( 10 );
+        enlace.password = ( password ) ? await bcrypt.hash( password, salt ) : null;
+
+        enlace.autor = req.usuario.id
+    }
 
     // * Almacena  en la DB
     try {
         await enlace.save();
         return res.json({ msg: `${ enlace.url }`});
-        next();
     } catch (error) {
         console.log(error);
     }
