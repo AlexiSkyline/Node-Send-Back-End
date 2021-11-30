11629d31ec016bd7fa5861fd64f055c38b615bad
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-29 18:45:34 -0600

    we create our model Usuarios

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ console.log( 'Comenzando Node Send' );
 // * Puertos de la app
 const port = process.env.PORT || 4000;
 
+// *Rutas de la APP
+app.use( '/api/usuarios', require( './routes/usuarios' ) );
+
 // Todo: Arranca la app
 app.listen( port, () => {
     console.log( `El servidor esta funcionando en el puerto ${ port }` );
