d12ee95e157fe0a0afced1890d2a044713092244
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-30 16:24:15 -0600

    add file upload function

--- controllers/archivosController.js
+++ controllers/archivosController.js
@@ -1,23 +1,31 @@
 const multer = require( 'multer' );
 const shortid = require( 'shortid' );
 
-const configuracionMulter = {
-    limits: { fileSize : 1000000 },
-    storage: fileStorage = multer.diskStorage({
-        destination: ( req, file, callback ) => {
-            callback( null, __dirname + '/../uploads' );
-        },
-        filename:  ( req, file, callback ) => {
-            const extension = file.mimetype.split( '/' )[1];
-            callback( null, `${ shortid.generate() }.${ extension }` );
-        },
-    })
-}
-
-const upload = multer( configuracionMulter ).single( 'archivo' );
-
 exports.subirArchivo = async ( req, res, next ) => {
+    const configuracionMulter = {
+        limits : { fileSize : req.usuario ? 1024 * 1024 * 10 : 1024 * 1024 },
+        storage: fileStorage = multer.diskStorage({
+            destination: ( req, file, callBack ) => {
+                callBack( null, __dirname+'/../uploads' );
+            },
+            filename: ( req, file, callBack ) => {
+                const extension = file.originalname.substring( file.originalname.lastIndexOf('.'), file.originalname.length );
+                callBack( null, `${shortid.generate()}${extension}` );
+            }
+        })
+    }
     
+    const upload = multer( configuracionMulter ).single( 'archivo' );
+
+    upload( req, res, async ( error ) => {
+        console.log( req.file );
+        
+        if( !error ) {
+            res.json({ archivos: req.file.filename });
+        } else {
+            console.log( error );
+        }
+    });
 }
 
 exports.eliminarArchivo = async ( req, res ) => {
