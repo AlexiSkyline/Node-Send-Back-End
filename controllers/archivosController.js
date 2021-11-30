a5c208ea5aaada0b43f47b56a26d81b8b48809d5
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-11-30 16:14:19 -0600

    add multer configuration

--- controllers/archivosController.js
+++ controllers/archivosController.js
@@ -1,7 +1,23 @@
+const multer = require( 'multer' );
+const shortid = require( 'shortid' );
 
+const configuracionMulter = {
+    limits: { fileSize : 1000000 },
+    storage: fileStorage = multer.diskStorage({
+        destination: ( req, file, callback ) => {
+            callback( null, __dirname + '/../uploads' );
+        },
+        filename:  ( req, file, callback ) => {
+            const extension = file.mimetype.split( '/' )[1];
+            callback( null, `${ shortid.generate() }.${ extension }` );
+        },
+    })
+}
 
-exports.subirArchivo = async ( req, res ) => {
+const upload = multer( configuracionMulter ).single( 'archivo' );
 
+exports.subirArchivo = async ( req, res, next ) => {
+    
 }
 
 exports.eliminarArchivo = async ( req, res ) => {
